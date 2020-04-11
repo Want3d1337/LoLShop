2395cc54681460e7b9068766691b408379adcd6b
Nikola Krasimirov Donev <Want3d1337@students.softuni.bg>
2020-04-11 13:49:13 +0300

    Accounts Page Added

---
+++ Web/LoLShop.Web/Controllers/AccountsController.cs
@@ -0,0 +1,18 @@
+﻿namespace LoLShop.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AccountsController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Web/LoLShop.Web/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿namespace LoLShop.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class UsersController : BaseController
+    {
+        [HttpGet]
+        [Route("/Users/Account")]
+        public async Task<IActionResult> AccountDetails()
+        {
+            return this.View();
+        }
+    }
+}
