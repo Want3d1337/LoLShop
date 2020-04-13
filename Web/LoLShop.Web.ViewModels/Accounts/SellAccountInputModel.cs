ea0a784388993606dc1b743272707f72920c4dc2
Nikola Krasimirov Donev <Want3d1337@students.softuni.bg>
2020-04-13 14:01:04 +0300

    Sell account page updated

---
+++ Web/LoLShop.Web.ViewModels/Accounts/SellAccountInputModel.cs
@@ -0,0 +1,30 @@
+﻿namespace LoLShop.Web.ViewModels.Accounts
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using LoLShop.Data.Models;
+    using LoLShop.Services.Mapping;
+
+    public class SellAccountInputModel : IMapTo<Account>
+    {
+        [Required]
+        public int ChampionsCount { get; set; }
+
+        [Required]
+        public int SkinsCount { get; set; }
+
+        [Required]
+        public int BlueEssence { get; set; }
+
+        [Required]
+        public int RiotPoints { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
--- Web/LoLShop.Web/Controllers/AccountsController.cs
+++ Web/LoLShop.Web/Controllers/AccountsController.cs
@@ -4,13 +4,20 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
+    [Authorize]
     public class AccountsController : BaseController
     {
         [HttpGet]
         public async Task<IActionResult> All()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Sell()
         {
             return this.View();
         }
