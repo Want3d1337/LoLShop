b9d4bad349536b901ca142d497157c50d6cac70f
Nikola Krasimirov Donev <Want3d1337@students.softuni.bg>
2020-04-14 22:01:07 +0300

    Apply View Page Changed

---
+++ Web/LoLShop.Web/Controllers/JobsController.cs
@@ -0,0 +1,14 @@
+﻿namespace LoLShop.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class JobsController : BaseController
+    {
+        public IActionResult Apply()
+        {
+            return this.View();
+        }
+    }
+}
